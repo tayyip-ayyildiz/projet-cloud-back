c01117d8f6fbc8a089c6dac371d2fe629794fa6e
WassimOulhadj <wassim.oulhadj@etu.univ-orleans.fr>
2024-11-26 10:01:43 +0100

    init project

---
+++ src/main/java/org/example/projetcloudback/ProjetCloudBackApplication.java
@@ -0,0 +1,13 @@
+package org.example.projetcloudback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetCloudBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetCloudBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/projetcloudback/ProjetCloudBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.projetcloudback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetCloudBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
